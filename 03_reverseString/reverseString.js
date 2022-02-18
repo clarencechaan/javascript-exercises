3f50586aa2750759462315cd3fe39a093af81c25
Clarence Chan <clarencechaan@icloud.com>
2022-02-18 18:46:59 -0500

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    resultString = "";
+
+    for (i = string.length - 1; i >= 0; i--) {
+        resultString += string[i];
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line
