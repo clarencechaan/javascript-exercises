697eebbd373071a62cf9216432c6ceab3cc1e4d7
Clarence Chan <clarencechaan@icloud.com>
2022-02-18 19:14:48 -0500

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let sum = 0;
+
+    // ensures a is the smaller variable
+    if (a > b) {
+        [a, b]  = [b, a];
+    }
+
+    // throws error if arg is negative or not a number
+    if (a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number') {
+        return 'ERROR'
+    }
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
