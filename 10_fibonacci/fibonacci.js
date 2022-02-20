08382e5e22d2fef773d7e92238945217b7f37b29
Clarence Chan <clarencechaan@icloud.com>
2022-02-20 14:52:13 -0500

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    const fibonacciSequence = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibonacciSequence[i] = fibonacciSequence[i - 2] + fibonacciSequence[i - 1];
+    }
+    return fibonacciSequence[num];
 };
 
 // Do not edit below this line
