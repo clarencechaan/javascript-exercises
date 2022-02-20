c3ab34ef8d055b099e7b1d50c335bca78a29df49
Clarence Chan <clarencechaan@icloud.com>
2022-02-20 14:45:52 -0500

    completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    let resultString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        resultString += string[i];
+    }
+
+    // strip punctuation, strips spaces, convert to lowercase
+    string = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()! ]/g,"");
+    resultString = resultString.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()! ]/g,"");
+
+    return string === resultString;
 
 };
 
