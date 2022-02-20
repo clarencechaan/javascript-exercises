a4fd4eec2e17d1836664f3c3c1fbf3ffbf247760
Clarence Chan <clarencechaan@icloud.com>
2022-02-20 15:13:19 -0500

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce(
+        (oldest, person) => {
+            let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+            let personAge = person.yearOfDeath - person.yearOfBirth;
 
+            const currentYear = (new Date()).getFullYear();
+            if (!('yearOfDeath' in oldest)) {
+                oldestAge = currentYear - oldest.yearOfBirth;
+            }
+            if (!('yearOfDeath' in person)) {
+                personAge = currentYear - personAge.yearOfBirth
+            }
+            
+            return (oldestAge > personAge) ? oldest : person;
+        }, people[0]);
 };
 
 // Do not edit below this line
